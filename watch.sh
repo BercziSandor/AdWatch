e5244c7da9c531ef2c4a2742324a765424fc26ba
Berczi, Sandor <berczi.sandor@gmail.com>
2017-08-15 04:53:31 +0200

    x

--- watch.sh
+++ watch.sh
@@ -4,7 +4,7 @@ thisDir=$(dirname $(readlink -f $0))
 cd $thisDir
 if ! pgrep get.pl >/dev/null; then 
 	echo "$(date) get.pl is not running, restart it" | tee -a $thisDir/watch.log
-	perl ./get.pl >> ./get.log &
+	# perl ./get.pl >> ./get.log &
 else
 	echo "$(date) get.pl is running." | tee -a $thisDir/watch.log
 fi
