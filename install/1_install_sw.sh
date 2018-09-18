faea01cbb1be9e8133c1693a1228e18e20e4e548
sberczi <Sandor.Berczi@t-systems.com>
2018-09-18 22:17:24 +0200

    final changes

--- install/1_install_sw.sh
+++ install/1_install_sw.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -euo pipefail
+set -e
+
 set -vx
 
 # call with user sanyi
 
-
-# install:
-this=https://raw.githubusercontent.com/BercziSandor/hasznaltAutoWatcher/master/install/0_install_sw.sh
 git config --global user.email "Berczi.Sandor@gmail.com"
 git config --global user.name "Berczi Sándor"
 
@@ -15,13 +14,17 @@ git config --global user.name "Berczi Sándor"
 # curl -L $this -o /tmp/$(basename $this)
 # OR bash <(curl -s $this)
 
+if [ "`whoami`" == "root" ]; then
+  echo "You must NOT use root for this, aborting."
+  exit 1
+fi
 
-apt -y update
-apt -y upgrade
-apt -y autoremove
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y autoremove
 
 # SWAP
-if [ ! -e ~/install_OK_swap ]; then
+if [ ! -e /swapfile ]; then
   swapon -s
   free -m
   fallocate -l 4G /swapfile
@@ -29,12 +32,10 @@ if [ ! -e ~/install_OK_swap ]; then
   mkswap /swapfile
   swapon /swapfile
   sh -c "echo '/swapfile   none    swap    sw    0   0' >> /etc/fstab"
-  touch ~/install_OK_swap
 fi
 
 # system tools
-apt -y install gcc make libssl-dev mailutils mc vim htop
-apt -y install perl-openssl-defaults
+sudo apt -y install gcc make libssl-dev mailutils mc vim htop perl-openssl-defaults zlib1g-dev
 timedatectl set-timezone Europe/Budapest
 
 # PERL
@@ -49,3 +50,5 @@ cpanm --sudo install LWP::UserAgent
 cpanm --sudo install LWP::Protocol::https
 cpanm --sudo install WWW::Mechanize
 cpanm --sudo install Email::Sender::Simple
+
+sudo apt -y upgrade
