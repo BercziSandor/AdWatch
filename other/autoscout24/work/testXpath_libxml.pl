053ced4ab90aab356038cf2f22c9781131b1f173
sberczi <Sandor.Berczi@t-systems.com>
2018-10-29 08:42:36 +0100

    sdf

--- other/autoscout24/work/testXpath_libxml.pl
+++ other/autoscout24/work/testXpath_libxml.pl
@@ -9,9 +9,9 @@ use XML::LibXML;
 my $filename = '1540465883__.html';
 
 my $dom = XML::LibXML->load_html(
-    	location  => $filename,
-    	recover   => 1,
-	suppress_errors => 1,
+  location        => $filename,
+  recover         => 1,
+  suppress_errors => 1,
 );
 
 # say $dom->toStringHTML();
@@ -19,33 +19,32 @@ my $dom = XML::LibXML->load_html(
 my $xpath;
 my $result;
 
-$xpath='//div[@id="resultlist"]';
-for my $resultListElement ( $dom->findnodes( $xpath ) ) {
-    foreach my $elementChild ( $resultListElement->getChildnodes ) {
-        if ( $elementChild->nodeType() == XML_ELEMENT_NODE ) {
-            say $elementChild->textContent(), " ";
-        }
+$xpath = '//div[@id="resultlist"]';
+for my $resultListElement ( $dom->findnodes($xpath) ) {
+  foreach my $elementChild ( $resultListElement->getChildnodes ) {
+    if ( $elementChild->nodeType() == XML_ELEMENT_NODE ) {
+      say $elementChild->nodeName . ": [" . $elementChild->textContent() . "]";
     }
-}
+  }
+} ### for my $resultListElement...
 
 say 'xxxxxxxxxxxxxxxxxxxxxxxx';
 
-$result=$dom->findnodes( $xpath );
+$result = $dom->findnodes($xpath);
 say '$result is a ', ref($result);
 my $i = 1;
-foreach my $i (1..$result->size) {
-    my $node = $result->get_node($i);
-    say '$node is a ', ref($node);
-    say "X: " . $node->nodeName if $node->nodeType == XML_ELEMENT_NODE;
+foreach my $i ( 1 .. $result->size ) {
+  my $node = $result->get_node($i);
+  say '$node is a ', ref($node);
+  say "X: " . $node->nodeName if $node->nodeType == XML_ELEMENT_NODE;
 }
 
 exit 0;
 
-
 $xpath = '//div[@id="resultlist"]/article[@itemtype="http://schema.org/Product"]/section[@class="content-section"]';
 
-$result=$dom->findnodes($xpath);
+$result = $dom->findnodes($xpath);
 
 foreach my $content ($result) {
-    say $content->findvalue('./span[@itemprop="name"]');
+  say $content->findvalue('./span[@itemprop="name"]');
 }
