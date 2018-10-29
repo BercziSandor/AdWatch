09cb5158492d2ae4c1f51a9ce00b4530f0883956
Berczi Sándor <Berczi.Sandor@gmail.com>
2018-10-29 08:27:54 +0100

    dfwq

--- other/autoscout24/work/testXpath_libxml.pl
+++ other/autoscout24/work/testXpath_libxml.pl
@@ -9,8 +9,35 @@ use XML::LibXML;
 my $filename = '1540465883__.html';
 
 my $dom = XML::LibXML->load_html(
-    location  => $filename,
-    recover   => 1,
+    	location  => $filename,
+    	recover   => 1,
+	suppress_errors => 1,
 );
 
-say $dom->toStringHTML();
+# say $dom->toStringHTML();
+
+my $xpath;
+my $result;
+
+$xpath='//div[@id="resultlist"]';
+$result=$dom->findnodes($xpath);
+$result=$result->childNodes;
+say '$result is a ', ref($result);
+my $i = 1;
+foreach my $i (1..$result->size) {
+    my $node = $result->get_node($i);
+    say $node->nodeName if $node->nodeType == XML_ELEMENT_NODE;
+}
+exit 0;
+
+
+$xpath = '//div[@id="resultlist"]/article[@itemtype="http://schema.org/Product"]/section[@class="content-section"]';
+
+$result=$dom->findnodes($xpath);
+
+foreach my $content ($result) {
+    say $content->findvalue('./span[@itemprop="name"]');
+}
+
+
+
