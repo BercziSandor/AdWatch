0df629179d20459069d2cd327f45bd725efc416d
sberczi <Sandor.Berczi@t-systems.com>
2018-10-31 05:46:51 +0100

    sdf

--- other/autoscout24/work/testXpath_libxml.pl
+++ other/autoscout24/work/testXpath_libxml.pl
@@ -47,8 +47,10 @@ my $xpath;
 $xpath='//div[@id="resultlist"]/article';
 my $items = $G_HTML_TREE->findnodes( $xpath ) or return 1;
 foreach my $item ( $items->get_nodelist ) {
+  $index++;
   $xpath = './section[@class="content-section"]//span[@itemprop="name"]';
   my $title = u_cleanString($item->findvalue($xpath));
+  say "########\n$index";
   say " title: [$title]";
 
   my $desc   = u_cleanString( $item->findvalue( './section[@class="content-section"]//div[@itemprop="description"]' ) );
@@ -72,44 +74,3 @@ foreach my $item ( $items->get_nodelist ) {
   my $link = $item->findvalue( './section[@class="content-section"]//div[contains(@class, "header")]/a/@href' );
   say " link: [$link]";
 }
-
-exit 0;
-
-
-my $result;
-my $articles = $G_HTML_TREE->findnodes('//div[@id="resultlist"]/article');
-
-for my $article (@$articles) {
-  my $contents = $article->findnodes('./section[@class="content-section"]');
-  next unless $contents->size;
-
-  for my $content (@$contents) {
-    my $name = u_cleanString( $G_HTML_TREE->findvalue( './/span[@itemprop="name"]', $content ) );
-    say "\n**************";
-    say " title: [$name]";
-
-    my $desc   = u_cleanString( $G_HTML_TREE->findvalue( './/div[@itemprop="description"]', $content ) );
-    my $yearKm = u_cleanString( $G_HTML_TREE->findvalue( './/span[@class="desc-left"]',     $content ) );
-
-    # 2008 75.000 km
-    my $year = $yearKm;
-    $year =~ s/^(\d*) .*/$1/;
-    my $age = 2018 - $year;
-    my $km  = $yearKm;
-    $km =~ s/^\d* (.*)/$1/;
-
-    my $price = u_cleanString( $G_HTML_TREE->findvalue( './/span[@class="pull-right"]', $content ) );
-    $price =~ s/,-/ €/;
-
-    my $text = "\n - $price\n - $year($age)\n - $km\n - $desc\n";
-    $text =~ s/bleifrei//g;
-    $text = u_clearSpaces($text);
-    say " text: [$text]";
-
-    my $link = $G_HTML_TREE->findvalue( './/div[contains(@class, "header")]/a/@href', $content );
-    say " link: [$link]";
-
-  } ### for my $content (@$contents)
-
-} ### for my $article (@$articles)
-exit 0;
