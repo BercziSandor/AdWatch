1c89675d05e87f06d1772ad524cae9f43d571324
sberczi <Sandor.Berczi@t-systems.com>
2018-12-14 09:51:20 +0100

    Minor changes: refactoring

--- other/autoscout24/watch.sh
+++ other/autoscout24/watch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # set -vx
-
+fileToStart=getAutoscout24.at.pl
 thisDir=$(dirname $(readlink -f $0))
 cd $thisDir
-if ps -aef | grep -v grep | grep perl | grep -q "getAutoscout24.at.pl"; then
-	echo "$(date) getAutoscout24.at.pl is running:" | tee -a $thisDir/watch.log
+if ps -aef | grep -v grep | grep perl | grep -q "$fileToStart"; then
+	echo "$(date) Nothing to do, $fileToStart is already running:" | tee -a $thisDir/watch.log
  	ps -aef | head -1                      | tee -a $thisDir/watch.log
- 	ps -aef | grep -v grep | grep perl | grep "getAutoscout24.at.pl" | tee -a $thisDir/watch.log
+ 	ps -aef | grep -v grep | grep perl | grep "$fileToStart" | tee -a $thisDir/watch.log
 else
-	echo "$(date) getAutoscout24.at.pl is not running, restart it" | tee -a $thisDir/watch.log
-	perl ./getAutoscout24.at.pl >> ./getAutoscout24.at.log &
+	echo "$(date) $fileToStart is not running, starting it" | tee -a $thisDir/watch.log
+	perl ./$fileToStart >> /dev/null &
 fi
