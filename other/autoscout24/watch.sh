8fda2d0ed915b12ad0d5de6e86f53b0c0d69dba6
sberczi <Sandor.Berczi@t-systems.com>
2018-12-14 10:56:59 +0100

    prep for merging willhaben -> autoscout

--- other/autoscout24/watch.sh
+++ other/autoscout24/watch.sh
@@ -3,11 +3,20 @@
 fileToStart=getAutoscout24.at.pl
 thisDir=$(dirname $(readlink -f $0))
 cd $thisDir
-if ps -aef | grep -v grep | grep perl | grep -q "$fileToStart"; then
-	echo "$(date) Nothing to do, $fileToStart is already running:" | tee -a $thisDir/watch.log
- 	ps -aef | head -1                      | tee -a $thisDir/watch.log
- 	ps -aef | grep -v grep | grep perl | grep "$fileToStart" | tee -a $thisDir/watch.log
-else
-	echo "$(date) $fileToStart is not running, starting it" | tee -a $thisDir/watch.log
-	perl ./$fileToStart >> /dev/null & 
-fi
+
+function watchFor
+{
+	site=$1
+	if ps -aef | grep -v grep | grep perl | grep -q "$fileToStart" | grep -q "-s $site" ; then
+		echo "$(date) Nothing to do, $fileToStart for $site is already running:" | tee -a $thisDir/watch.log
+	 	ps -aef | head -1                      | tee -a $thisDir/watch.log
+	 	ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "-s $site" | tee -a $thisDir/watch.log
+	else
+		echo "$(date) $fileToStart isn't running for $site, starting it" | tee -a $thisDir/watch.log
+		perl ./$fileToStart -s $site >> /dev/null &
+	fi
+}
+
+watchFor willHaben
+sleep 15
+watchFor autoScout24
