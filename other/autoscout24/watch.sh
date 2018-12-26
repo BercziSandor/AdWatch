1421b514fb8a48541fb214a886ecb38f4e3f57e3
sberczi <Sandor.Berczi@t-systems.com>
2018-12-26 10:00:10 +0100

    fix

--- other/autoscout24/watch.sh
+++ other/autoscout24/watch.sh
@@ -7,10 +7,10 @@ cd $thisDir
 function watchFor
 {
 	site=$1
-	if ps -aef | grep -v grep | grep perl | grep -q "$fileToStart" | grep -q "-s $site" ; then
+	if ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "s $site" ; then
 		echo "$(date) Nothing to do, $fileToStart for $site is already running:" | tee -a $thisDir/watch.log
-	 	ps -aef | head -1                      | tee -a $thisDir/watch.log
-	 	ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "-s $site" | tee -a $thisDir/watch.log
+	 	 ps -aef | head -1                      | tee -a $thisDir/watch.log
+	 	 ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "s $site" | tee -a $thisDir/watch.log
 	else
 		echo "$(date) $fileToStart isn't running for $site, starting it" | tee -a $thisDir/watch.log
 		perl ./$fileToStart -s $site >> /dev/null &
