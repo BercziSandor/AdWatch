275ac5ca56202036f053ce13ec5c6a7e28601ef9
sberczi <Sandor.Berczi@t-systems.com>
2019-01-21 18:52:18 +0100

    stop function

--- watch.sh
+++ watch.sh
@@ -10,15 +10,24 @@ function watchFor
 {
 	site=$1
 	shift
-	if ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "s $site" ; then
-		echo "$(date) Nothing to do, $fileToStart for $site is already running:" | tee -a $thisDir/watch.log
-	 	 ps -aef | head -1                      | tee -a $thisDir/watch.log
-	 	 ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "s $site" | tee -a $thisDir/watch.log
+	if [ "$1" == "stop" ]; then
+		if ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "s $site" ; then
+			pid=$(ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "s $site" | awk '{print $2}')
+			if [ ! -z $pid ]; then
+				kill -9 $pid
+			fi
+		fi
 	else
-		echo "$(date) $fileToStart does not run for $site, starting it" | tee -a $thisDir/watch.log
-		set -vx
-		perl ./$fileToStart -s $site $* 2>> ${site}.err.log 1> /dev/null &
-		set +vx
+		if ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "s $site" ; then
+			echo "$(date) Nothing to do, $fileToStart for $site is already running:" | tee -a $thisDir/watch.log
+		 	 ps -aef | head -1                      | tee -a $thisDir/watch.log
+		 	 ps -aef | grep -v grep | grep perl | grep "$fileToStart" | grep -q "s $site" | tee -a $thisDir/watch.log
+		else
+			echo "$(date) $fileToStart does not run for $site, starting it" | tee -a $thisDir/watch.log
+			set -vx
+			perl ./$fileToStart -s $site $* 2>> ${site}.err.log 1> /dev/null &
+			set +vx
+		fi
 	fi
 }
 watchFor willHaben $*
